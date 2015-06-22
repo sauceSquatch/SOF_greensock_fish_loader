a39e44c49d6abbab01c28093cdcd68f6fdfe6708
Jamie Naioti <Jamie.Naioti@razorfish.com>
2015-06-22 12:00:54 -0500

    added an onComplete to pause tweens so nothing is running in the background

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -19,21 +19,21 @@ initBuild = function() {
 }
 
 animate_buildOn = function() {
-  build_TL = new TimelineMax();
+  build_TL = new TimelineMax({delay:0.2, onComplete:killTweens});
   build_TL.to(loader_container, 1.05, {width:250, height:250, top:0, ease:Elastic.easeOut});
-  build_TL.to(loader_container, 1.05, {width:2, height:2, top:50, ease:Elastic.easeIn, delay:10});
+  build_TL.to(loader_container, 1.05, {width:2, height:2, top:50, ease:Elastic.easeIn, delay:5.5});
 }
 
 animate_wave = function() {
-  wave_timeline = new TimelineMax({repeat:-1});
-  wave_timeline.to(wave, 5, {"left":-900, ease:Power0.easeNone});
+  wave_TL = new TimelineMax({repeat:-1});
+  wave_TL.to(wave, 5, {"left":-900, ease:Power0.easeNone});
 }
 
 animate_fish = function(){
   // console.log("fish: ", fish);
-  fish_timeline = new TimelineMax({repeat:-1});
-  fish_timeline.set(fish, {"left":240, "top":120, scale:0.15, rotation:230});
-  fish_timeline.to(fish, 1.25, {
+  fish_TL = new TimelineMax({repeat:-1});
+  fish_TL.set(fish, {"left":240, "top":120, scale:0.15, rotation:230});
+  fish_TL.to(fish, 1.25, {
     bezier:{
       type:"thru", 
       values:[{"left":240, "top":120}, 
@@ -42,5 +42,12 @@ animate_fish = function(){
       autoRotate:true
     },
     ease:SlowMo.ease.config(0.5, 0.5, false), delay:0.25, scale:1.35});
-  // fish_timeline.to(ripple, 1.25, {scale:2, alpha:0, ease:Power4.easeOut});
+  // fish_TL.to(ripple, 1.25, {scale:2, alpha:0, ease:Power4.easeOut});
+}
+
+killTweens = function() {
+  console.log("killTweens", build_TL);
+  build_TL.pause();
+  wave_TL.pause();
+  fish_TL.pause();
 }
